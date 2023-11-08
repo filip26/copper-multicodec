e523523eedeb755ebdd8df80b6c8c280a42dc512
Filip <filip26@gmail.com>
2023-11-08 18:35:14 +0100

    Simplify coder implementation

--- src/main/java/com/apicatalog/multicodec/Multicoder.java
+++ src/main/java/com/apicatalog/multicodec/Multicoder.java
@@ -1,11 +1,9 @@
 package com.apicatalog.multicodec;
 
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import com.apicatalog.multicodec.Multicodec.Tag;
 import com.apicatalog.uvarint.UVarIntInputStream;
 
 /**
@@ -38,24 +36,13 @@ public final class Multicoder {
     }
 
     /**
-     * Finds key codec in the registry if exists
-     * 
-     * @param code a byte array identifying a multicodec
-     * @return key codec or an empty {@link Optional} if the multicodec does not
-     *         exist
-     */
-    private Optional<Multicodec> findKey(long code) {
-        return Optional.ofNullable(codecs.get(code));
-    }
-
-    /**
      * Finds a codec in the registry if exists
      * 
-     * @param type    a multicodec type
      * @param encoded a byte array identifying a multicodec
+     * 
      * @return a codec or an empty {@link Optional} if the multicodec does not exist
      */
-    public Optional<Multicodec> getCodec(final Tag type, final byte[] encoded) {
+    public Optional<Multicodec> getCodec(final byte[] encoded) {
 
         if (encoded == null) {
             throw new IllegalArgumentException("The encoded value must not be null.");
@@ -65,39 +52,22 @@ public final class Multicoder {
             throw new IllegalArgumentException("The encoded value be non empty byte array.");
         }
 
-        switch (type) {
-        case Key:
-            final long code = UVarIntInputStream.readLong(encoded);
-            
-            return findKey(code);
-
-        default:
-            break;
-        }
-
-        throw new IllegalArgumentException("Unsupported type [" + type + "].");
+        final long code = UVarIntInputStream.readLong(encoded);
+        
+        return Optional.ofNullable(codecs.get(code));
     }
 
     /**
      * Decode an encoded value
      * 
-     * @param type    multicodec type used to encode the value
      * @param encoded to decode
      * @return a decoded value
      * 
      * @throws IllegalArgumentException if the value cannot be decoded
      */
-    public byte[] decode(final Tag type, final byte[] encoded) throws IllegalArgumentException {
-        return getCodec(type, encoded)
+    public byte[] decode(final byte[] encoded) throws IllegalArgumentException {
+        return getCodec(encoded)
                 .map(codec -> codec.decode(encoded))
                 .orElseThrow(() -> new IllegalArgumentException("Unsupported multicode encoding [" + String.format("0x%hh, 0x%hh, ...", encoded[0], encoded[1]) + "]."));
-    }
-
-    public final static String getKey(byte[] varint) {
-        final StringWriter writer = new StringWriter(varint.length * 2);
-        for (int i = 0; i < varint.length; i++) {
-            writer.write(String.format("%02x", varint[i]));
-        }
-        return writer.toString();
     }
 }

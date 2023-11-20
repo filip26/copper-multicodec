a732299b5bb0e10199f25405eff40e009653f363
Filip <filip26@gmail.com>
2023-11-20 21:58:22 +0100

    Minor improvement

--- src/main/java/com/apicatalog/multicodec/Multicoder.java
+++ src/main/java/com/apicatalog/multicodec/Multicoder.java
@@ -3,7 +3,6 @@ package com.apicatalog.multicodec;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -40,12 +39,11 @@ public final class Multicoder {
      */
     public static Multicoder getInstance(Tag... tags) {
         return new Multicoder(
-                new TreeMap<>(
-                        MulticodecRegistry.CODECS.values().stream()
-                                .filter(codec -> tags.length == 1
-                                        ? tags[0] == codec.tag()
-                                        : Arrays.stream(tags).anyMatch(tag -> tag == codec.tag()))
-                                .collect(Collectors.toMap(Multicodec::code, Function.identity()))));
+                MulticodecRegistry.CODECS.values().stream()
+                        .filter(codec -> tags.length == 1
+                                ? tags[0] == codec.tag()
+                                : Arrays.stream(tags).anyMatch(tag -> tag == codec.tag()))
+                        .collect(Collectors.toMap(Multicodec::code, Function.identity())));
     }
 
     /**
